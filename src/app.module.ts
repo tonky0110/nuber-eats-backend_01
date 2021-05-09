935b4dd9588c0b95a0481bc7fa0910b3e6a86602
tonky0110 <tonky0110@gmail.com>
2021-05-10 00:07:25 +0900

    0.0.2.2 .env를 이용한 환경설정 변수 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,18 +10,20 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot({
-      "type": "postgres",
-      "host": "localhost",
-      "port": 5432,
-      "username": "tonky0110",
-      "database": "nuber-eats",
-      "synchronize": true,
-      "logging": true,
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule
   ],
