d3514e1864851a20a0cd8beda684d54590d66c3a
tonky0110 <tonky0110@gmail.com>
2021-05-09 23:56:32 +0900

    0.0.2.1 configuration설정(nestjs/config, cross-env설치)

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 The Backend of Nuber Eats Clone
 
 ## 1.0 backend 구성
+
 ### Graphql
-   - npm i @nestjs/graphql graphql-tools graphql apollo-server-express
-   - code firest(autoSchemaFile: join(process.cwd(), 'src/schema.gql'),)
-   
+
+- npm i @nestjs/graphql graphql-tools graphql apollo-server-express
+- code firest(autoSchemaFile: join(process.cwd(), 'src/schema.gql'),)
 
 ### TypeORM
-  - npm i @nestjs/typeorm typeorm pg
+
+- npm i @nestjs/typeorm typeorm pg
+- npm i cross-env: 가상변수를 설정할 수 있도록 하는 라이브러리
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
@@ -6,6 +7,10 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
