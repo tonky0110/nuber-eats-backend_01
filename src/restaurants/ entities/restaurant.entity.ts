38dd2275f2834f17529730ac8a913c7b4ea14567
tonky0110 <tonky0110@gmail.com>
2021-05-10 23:58:04 +0900

    0.0.2.4 DB연결을 위한 service.ts생성과 연결(module.ts)

--- src/restaurants/ entities/restaurant.entity.ts
+++ src/restaurants/ entities/restaurant.entity.ts
@@ -1,16 +1,26 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@ObjectType()
+@ObjectType() // for Graphql
+@Entity() // for TypeORM
 export class Restaurant {
-    @Field(type => String)
+    @Field(type => Number) // for Graphql
+    @PrimaryGeneratedColumn() // for typeORM
+    id: number
+
+    @Field(type => String) // for Graphql
+    @Column() // form typeORM
     name: string;
 
-    @Field(type => Boolean)
+    @Field(type => Boolean) // for Graphql
+    @Column() // form typeORM
     isVegan?: boolean;
 
-    @Field(type => String)
+    @Field(type => String) // for Graphql
+    @Column() // form typeORM
     address: string;
 
-    @Field(type => String)
+    @Field(type => String) // for Graphql
+    @Column() // form typeORM
     ownerName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,12 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { Restaurant } from "./ entities/restaurant.entity";
 import { CreateRestaurantDto } from "./dto/create-restaurant.dto";
+import { RestaurantService } from "./restaurants.service";
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
+    constructor(private readonly restaurantService: RestaurantService){}
+
     @Query(returns => [Restaurant])
     restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
         return [];
