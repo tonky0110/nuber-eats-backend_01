8cd54f5b7450535f7d6bcd0158d2b8b2ffefe567
tonky0110 <tonky0110@gmail.com>
2021-05-11 23:35:50 +0900

    0.0.3.3 defaultValue와 nullable(for Graphql)

--- src/restaurants/ entities/restaurant.entity.ts
+++ src/restaurants/ entities/restaurant.entity.ts
@@ -1,5 +1,5 @@
 import { Field, InputType, ObjectType } from "@nestjs/graphql";
-import { IsBoolean, IsNumber, IsString } from "class-validator";
+import { IsBoolean, IsNumber, IsOptional, IsString, Length, minLength } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @InputType({ isAbstract: true })
@@ -8,31 +8,33 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 export class Restaurant {
     @Field(type => Number) // for Graphql
     @PrimaryGeneratedColumn() // for typeORM
-    @IsNumber()
+    @IsNumber() // for ValidationCheck
     id: number
 
     @Field(type => String) // for Graphql
     @Column() // for typeORM
-    @IsString()
+    @Length(5) // for ValidationCheck
+    @IsString() // for ValidationCheck
     name: string;
 
-    @Field(type => Boolean) // for Graphql
-    @Column() // for typeORM
-    @IsBoolean()
+    @Field(type => Boolean, { nullable: true }) // for Graphql
+    @Column({ default: false }) // for typeORM
+    @IsOptional() // for ValidationCheck
+    @IsBoolean() // for ValidationCheck
     isVegan?: boolean;
 
-    @Field(type => String) // for Graphql
+    @Field(type => String, { defaultValue : '강남' }) // for Graphql defaultValue로하면 해당값을 자동으로 보냄, nullable로 하면 값을 보내지 않음.
     @Column() // for typeORM
-    @IsString()
+    @IsString() // for ValidationCheck
     address: string;
 
-    @Field(type => String) // for Graphql
-    @Column() // for typeORM
-    @IsString()
-    ownerName: string;
+    // @Field(type => String) // for Graphql
+    // @Column() // for typeORM
+    // @IsString() // for ValidationCheck
+    // ownerName: string;
 
-    @Field(type => String) // for Graphql
-    @Column() // for typeORM
-    @IsString()
-    categoryName: string;
+    // @Field(type => String) // for Graphql
+    // @Column() // for typeORM
+    // @IsString() // for ValidationCheck
+    // categoryName: string;
 }
