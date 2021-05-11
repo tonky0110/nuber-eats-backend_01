83a94867df55581624c27e5ee9428370a86e14ef
tonky0110 <tonky0110@gmail.com>
2021-05-11 22:51:35 +0900

    0.0.3.1 create Restaurant with DTO

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -13,9 +13,15 @@ export class RestaurantResolver {
     }
     
     @Mutation(returns => Boolean)
-    createRestaurant(
-        @Args() CreateRestaurantDto: CreateRestaurantDto
-    ): boolean {
-        return true;
+    async createRestaurant(
+        @Args() createRestaurantDto: CreateRestaurantDto
+    ): Promise<boolean> {
+        try {
+            this.restaurantService.createRestaurant(createRestaurantDto);
+            return true;
+        } catch(e) {
+            console.log(e);
+            return false;
+        }
     }
 }
