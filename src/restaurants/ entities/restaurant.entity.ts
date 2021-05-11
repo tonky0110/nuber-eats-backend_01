24f426325cb10ff56982993c0742de157b8135ac
tonky0110 <tonky0110@gmail.com>
2021-05-11 23:13:57 +0900

    0.0.3.2 OmitType을 이용해 dto생성(from entity)

--- src/restaurants/ entities/restaurant.entity.ts
+++ src/restaurants/ entities/restaurant.entity.ts
@@ -1,26 +1,38 @@
-import { Field, ObjectType } from "@nestjs/graphql";
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { IsBoolean, IsNumber, IsString } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@InputType({ isAbstract: true })
 @ObjectType() // for Graphql
 @Entity() // for TypeORM
 export class Restaurant {
     @Field(type => Number) // for Graphql
     @PrimaryGeneratedColumn() // for typeORM
+    @IsNumber()
     id: number
 
     @Field(type => String) // for Graphql
     @Column() // for typeORM
+    @IsString()
     name: string;
 
     @Field(type => Boolean) // for Graphql
     @Column() // for typeORM
+    @IsBoolean()
     isVegan?: boolean;
 
     @Field(type => String) // for Graphql
     @Column() // for typeORM
+    @IsString()
     address: string;
 
     @Field(type => String) // for Graphql
     @Column() // for typeORM
+    @IsString()
     ownerName: string;
+
+    @Field(type => String) // for Graphql
+    @Column() // for typeORM
+    @IsString()
+    categoryName: string;
 }
