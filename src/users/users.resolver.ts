9aa25f85d7d73bbf570a9a67ae583b817b72a356
tonky0110 <tonky0110@gmail.com>
2021-05-12 22:24:21 +0900

    0.0.4.1 resolvers, service && query

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,16 @@
+import { Query } from "@nestjs/graphql";
+import { Resolver } from "@nestjs/graphql";
+import { User } from "./entities/user.entity";
+import { UsersService } from "./users.service";
+
+@Resolver(of => User)
+export class UsersResolver {
+    constructor(
+        private readonly userService: UsersService
+    ) {}
+
+    @Query(returns => Boolean)
+    hi() {
+        return true;
+    }
+}
