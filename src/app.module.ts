2489cb61de0f0745987f1d6e575c7bfca8785c27
tonky0110 <tonky0110@gmail.com>
2021-05-12 22:03:14 +0900

    0.0.4.0 nest g mo users, common - create User table extends CoreEntity

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { Restaurant } from './restaurants/ entities/restaurant.entity';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -34,9 +37,10 @@ import { Restaurant } from './restaurants/ entities/restaurant.entity';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
-      entities: [Restaurant]
+      entities: [User]
     }),
-    RestaurantsModule
+    UsersModule,
+    CommonModule
   ],
   controllers: [],
   providers: [],
---
+++ src/common/entities/core.entity.ts
@@ -0,0 +1,13 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class CoreEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
