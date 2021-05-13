015a9103cdd597642acadb0b3fd9ad17dc627ca1
tonky0110 <tonky0110@gmail.com>
2021-05-13 22:45:59 +0900

    0.0.5.0 jsonwebtoken, @type/jsonwebtoken 설치 및 token생성(randomkeygen.com)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import * as jwt from 'jsonwebtoken';
 import { CreateAccountInput } from "./dtos/create-account.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class UsersService {
     constructor(
-        @InjectRepository(User) private readonly users : Repository<User>
+        @InjectRepository(User) private readonly users : Repository<User>,
+        private readonly config: ConfigService
     ) {}
 
     async createAccount({ email, password, role }: CreateAccountInput): Promise<{ok: boolean, error?: string}> {
@@ -58,9 +61,10 @@ export class UsersService {
             }
 
             // 3. jwt 토큰생성.
+            const token = jwt.sign({id: user.id}, this.config.get('SECRET_KEY'));
             return {
                 ok: true,
-                token: 'lalalalala'
+                token
             }
 
         } catch (error) {
