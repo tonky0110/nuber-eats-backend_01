1d6700ab630f1947b243f5f96227c919b3157c45
tonky0110 <tonky0110@gmail.com>
2021-05-13 23:09:21 +0900

    0.0.5.1 jwtModle을 Global모듈로 선언 후 usersService 생성자에서 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { Restaurant } from './restaurants/ entities/restaurant.entity';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { User } from './users/entities/user.entity';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
   imports: [
@@ -41,7 +42,8 @@ import { User } from './users/entities/user.entity';
       entities: [User]
     }),
     UsersModule,
-    CommonModule
+    CommonModule,
+    JwtModule.forRoot()
   ],
   controllers: [],
   providers: [],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,13 +6,17 @@ import { CreateAccountInput } from "./dtos/create-account.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
 import { ConfigService } from "@nestjs/config";
+import { JwtService } from "src/jwt/jwt.service";
 
 @Injectable()
 export class UsersService {
     constructor(
         @InjectRepository(User) private readonly users : Repository<User>,
-        private readonly config: ConfigService
-    ) {}
+        private readonly config: ConfigService,
+        private readonly jwtService: JwtService
+    ) {
+        // jwtService.hello();
+    }
 
     async createAccount({ email, password, role }: CreateAccountInput): Promise<{ok: boolean, error?: string}> {
         try{
