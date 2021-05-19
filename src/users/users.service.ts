5502a00204edff962a161627e73a7b27e3b24cff
tonky0110 <tonky0110@gmail.com>
2021-05-20 00:21:40 +0900

    0.0.5.11-3 Update Edit Profile with save() 1. typeorm에서 update는 db로 query만 호출하는 방식으로 BeforeUpdate()를 실행하지 못함. 2. findOne(id) -> user.email = email, user.password = password 후에 save()를 이용해 update를 처리해 주어야 한다.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -83,7 +83,15 @@ export class UsersService {
         return this.users.findOne({ id });
     }
 
-    async editProfile(userId: number, editProfileInput : EditProfileInput) {
-        return this.users.update(userId, { ...editProfileInput });
+    async editProfile(userId: number, { email, password }: EditProfileInput) {
+        // return this.users.update(userId, { ...editProfileInput });
+        const user = await this.users.findOne(userId);
+        if (email) {
+            user.email = email;
+        }
+        if (password) {
+            user.password = password;
+        }
+        return this.users.save(user);
     }
 }
